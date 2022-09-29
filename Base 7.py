1d61a192bc179683859d947267ed8f6eb775d6aa
Molly-12 <92814851+Molly-12@users.noreply.github.com>
2022-09-29 19:38:33 +0530

    Create Base 7.py

---
+++ Base 7.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def convertToBase7(self, num):
+      s=''
+      copy=num     //create a num copy
+      if num==0:
+        return '0'
+      if num<0:
+        num=abs(num)   //if negative number, convert it to positive using the abs function
+      while(num>0):
+        rem=num%7
+        num=num//7
+        s=s+str(rem)      //append the answer in the string
+      if(num>=0):
+        return s[::-1]
+      else:
+        return '-1'+s[::-1]
