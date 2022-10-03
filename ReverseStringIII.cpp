e2fc23abcf95eda47e01156a4d532deba0997950
Molly-12 <92814851+Molly-12@users.noreply.github.com>
2022-10-03 09:14:01 +0530

    Create ReverseStringIII.cpp

---
+++ ReverseStringIII.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+      int l,r=0;
+      while(l<s.size()){
+        while(r<s.size() && s[r]!=' ')
+          r++;
+        reverse(s.begin()+l,s.begin()+r);
+      }
+      return s;
+    }
+};
