e7f4fbff1c5c6643a2e1e72198fa62ea9a9bc030
Molly-12 <92814851+Molly-12@users.noreply.github.com>
2022-11-08 19:47:25 +0530

    Create Longest Palindromic Substring.java

---
+++ Longest Palindromic Substring.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String longestPalindrome(String s) {
+      if(s==null || s.length()<1)
+        return "";
+      int start=0;
+      int end=0;
+      for(int i=0;i<s.length();i++){
+        int len1=expandString(s,i,i);
+        int len2=expandString(s,i,i+1);
+        int len=Math.max(len1,len2);
+        
+        if(len>start-end){
+          start=i-((len-1)/2);
+          end=i+(len/2);
+        }
+        return s.substring(start,end+1);
+    }
+    public int expandString(String s,int left,int right){
+      if(s==null || left<right)
+        return 0;
+      if(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right){
+        left--;
+        right++;
+      }
+         return right-left-1;
+         }
+         }
+      
+         
+      
+      
+        
+        
+}
