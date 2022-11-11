69905d0720cd249449782392908782901d1203bd
Molly-12 <92814851+Molly-12@users.noreply.github.com>
2022-11-11 09:04:21 +0530

    Create Number of Islands.java

---
+++ Number of Islands.java
@@ -0,0 +1,38 @@
+/*Input:
+grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1*/
+
+
+class Solution {
+    public int numIslands(char[][] grid) {
+      int count=0;
+      
+      for(int i=0;i<grid.length;i++){
+        for(int j=0;j<grid[0].length;j++){
+          if(grid[i][j]=='1'){
+            count++;
+            bfs(grid,i,j);
+          }
+        }
+      }
+      return count;
+    }
+  
+  public void bfs(char[][] grid,int i,int j){
+    if(i<0 || i>=grid.length || j<0 || j>=grid.length || grid[i][j]=='0')
+      return;
+    grid[i][j]='0';
+    bfs(grid,i+1,j);
+    bfs(grid,i-1,j);
+    bfs(grid,i,j+1);
+    bfs(grid,i,j-1);
+  }
+}
+
+    
+    
